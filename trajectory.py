a5dd9c267533903bb7ab1a284ef967ee19551cf7
Aadi Umrani <aadi.p.um@gmail.com>
2023-04-30 20:34:55 +0300

    modified the file to incorporate a GUI using the tkinter library

--- trajectory.py
+++ trajectory.py
@@ -1,10 +1,9 @@
 import math
+from tkinter import ttk
+from tkinter import *
+import tkinter as tk
 import matplotlib.pyplot as plt
 import numpy as np
-
-def pequation(angleinit, vinit, g):
-    angle_rad = math.radians(angleinit)
-    print(f"Path Equation: {math.tan(angle_rad)}x - {(g / 2 * (vinit * math.cos(angle_rad)) ** 2)}x^2")
 
 def tflight(angleinit, vinit, g):
     angle_rad = math.radians(angleinit)
@@ -43,20 +42,46 @@ def plot_trajectory(angleinit, vinit, g):
 
 
 g = 9.81
-again = 'y'
 
-while again == 'y' or again == 'Y':
-    vinit = float(input("Enter the initial velocity (m/s): "))
-    angleinit = float(input("Enter the angle of projection (degrees): "))
+vinit = 0
+angleinit = 0
 
-    print("=======================================================")
-    pequation(angleinit, vinit, g)
-    print("Flight time (s): ", tflight(angleinit, vinit, g))
-    print("Maximum height (m): ", hmax(angleinit, vinit, g))
-    print("Horizontal range (m): ", hrange(angleinit, vinit, g))
-    print("Maximum possible range (m): ", rangemax(vinit, g))
-    print("=======================================================")
+win_input = tk.Tk()
+
+frm_v = tk.Frame(padx=30, pady=10)
+frm_a = tk.Frame(padx=30, pady=10)
+frm_btn = tk.Frame(padx=30, pady= 10)
+lbl_v = tk.Label(master = frm_v, font=15, text="Enter the initial velocity (m/s): ")
+lbl_a = tk.Label(master = frm_a, font=15, text="Enter the angle of projection (deg): ")
+ent_v = tk.Entry(master = frm_v, font=15, borderwidth=2)
+ent_a = tk.Entry(master = frm_a, font=15, borderwidth=2)
+btn_calc = tk.Button(font = 15, master = frm_btn, text="Calculate Trajectory", pady=5, padx=10)
+
+frm_v.pack()
+lbl_v.pack()
+ent_v.pack()
+frm_a.pack()
+lbl_a.pack()
+ent_a.pack()
+frm_btn.pack()
+btn_calc.pack()
+
+def traj_calc(event):
+    global vinit, angleinit
+    vinit = float(ent_v.get())
+    angleinit = float(ent_a.get())
+    lbl_ft = tk.Label(text="Flight time (s): " + str(tflight(angleinit, vinit, g)))
+    lbl_mh = tk.Label(text="Maximum height (m): " + str(hmax(angleinit, vinit, g)))
+    lbl_hr = tk.Label(text="Horizontal range (m): " + str(hrange(angleinit, vinit, g)))
+    lbl_mpr = tk.Label(text="Maximum possible range (m): " + str(rangemax(vinit, g)))
+    ttk.Separator(orient=HORIZONTAL, style='TSeparator', class_= ttk.Separator, takefocus= 1, cursor='plus').pack(fill=X, padx=10, expand=True)
+    lbl_ft.pack()
+    lbl_mh.pack()
+    lbl_hr.pack()
+    lbl_mpr.pack()
+    
     plot_trajectory(angleinit, vinit, g)
-    again = input("Calculate another trajectory? (y/n): ")
 
-print("Exiting program...")
+btn_calc.bind("<Button-1>", traj_calc)
+
+win_input.mainloop()
